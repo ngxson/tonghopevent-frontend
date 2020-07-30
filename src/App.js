84db0cdaf8dab8766959c5be42474e366f64e01a
ngxson <thichthat@gmail.com>
2020-07-30 13:01:19 +0200

    v4.8

--- src/App.js
+++ src/App.js
@@ -64,9 +64,9 @@ class Main extends React.Component {
 
   Copyright = () => {
     return <center>
-      Technology provided by&nbsp;
+      Developed by&nbsp;
       <b><a href="https://ngxson.com" target="_blank" rel="noopener noreferrer">Nui</a></b><br/>
-      Version 4.7
+      Version 4.8
       <br/><br/>
     </center>
   }
